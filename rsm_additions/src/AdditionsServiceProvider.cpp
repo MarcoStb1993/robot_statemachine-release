5e7856f130b393f48650ddc70f9338ff73b10851
Marco Steinbrink <marco.stb1993@gmail.com>
2020-11-04 08:51:36 +0100

    Rebase from 'upstream'

--- rsm_additions/src/AdditionsServiceProvider.cpp
+++ rsm_additions/src/AdditionsServiceProvider.cpp
@@ -7,17 +7,12 @@ AdditionsServiceProvider::AdditionsServiceProvider() :
 	ros::NodeHandle private_nh("~");
 	private_nh.param<std::string>("autonomy_cmd_vel_topic",
 			_autonomy_cmd_vel_topic, "/autonomy/cmd_vel");
-	std::string navigation_plugin;
-	private_nh.param<std::string>("navigation_plugin", navigation_plugin, "");
-	if (navigation_plugin.compare("rsm::NavigationState") == 0) {
-		_navigation_plugin_used = true;
-	} else {
-		_navigation_plugin_used = false;
-	}
 
 	ros::NodeHandle nh("rsm");
 
-	if (_navigation_plugin_used) {
+	std::string navigation_plugin;
+	private_nh.param<std::string>("navigation_plugin", navigation_plugin, "");
+	if (navigation_plugin.compare("rsm::NavigationState") == 0) {
 		_set_navigation_to_reverse_service = nh.advertiseService(
 				"setNavigationToReverse",
 				&AdditionsServiceProvider::setNavigationToReverse, this);
@@ -30,25 +25,45 @@ AdditionsServiceProvider::AdditionsServiceProvider() :
 				_autonomy_cmd_vel_topic, 10);
 	}
 
-	std::string mapping_plugin;
-	private_nh.param<std::string>("mapping_plugin", mapping_plugin, "");
-	if (mapping_plugin.compare("rsm::KinectMappingState") == 0) {
-		_reset_kinect_position_serivce = nh.advertiseService(
-				"resetKinectPosition",
-				&AdditionsServiceProvider::resetKinectPosition, this);
-		_kinetic_joint_controller = _nh.advertise<std_msgs::Float64>(
-				"kinetic_controller/command", 1, true);
+	std::string calculate_goal_plugin;
+	private_nh.param<std::string>("calculate_goal_plugin",
+			calculate_goal_plugin, "");
+	if (calculate_goal_plugin.compare("rsm::CalculateGoalState") == 0) {
+		_calculate_goal_plugin_used = true;
+		private_nh.param<double>("exploration_goal_tolerance",
+				_exploration_goal_tolerance, 0.05);
+		as = new MoveBaseActionServer(_nh, "frontier_move_base",
+				boost::bind(&AdditionsServiceProvider::navigationGoalCallback,
+						this, _1), false);
+		as->start();
+		frontiers_marker_array_subscriber = _nh.subscribe("explore/frontiers",
+				1, &AdditionsServiceProvider::frontierCallback, this);
+		_exploration_goals_publisher = nh.advertise<geometry_msgs::PoseArray>(
+				"explorationGoals", 1);
+		_exploration_mode_subscriber = nh.subscribe("explorationMode", 1,
+				&AdditionsServiceProvider::explorationModeCallback, this);
+		_failed_goals_publisher = nh.advertise<geometry_msgs::PoseArray>(
+				"failedGoals", 1);
+		_exploration_goal_subscriber = nh.subscribe("explorationGoalStatus", 1,
+				&AdditionsServiceProvider::explorationGoalCallback, this);
+		_get_navigation_goal_service = nh.serviceClient<
+				rsm_msgs::GetNavigationGoal>("getNavigationGoal");
+	} else {
+		_calculate_goal_plugin_used = false;
 	}
 
-	as = new MoveBaseActionServer(_nh, "frontier_move_base",
-			boost::bind(&AdditionsServiceProvider::navigationGoalCallback, this,
-					_1), false);
-	as->start();
+	std::string mapping_plugin;
+	private_nh.param<std::string>("mapping_plugin", mapping_plugin, "");
+	if (mapping_plugin.compare("rsm::RealsenseMappingState") == 0) {
+		_reset_realsense_position_serivce = nh.advertiseService(
+				"resetRealsensePosition",
+				&AdditionsServiceProvider::resetRealsensePosition, this);
+		_realsense_joint_controller = _nh.advertise<std_msgs::Float64>(
+				"realsense_controller/command", 1, true);
+	}
 
-	frontiers_marker_array_subscriber = _nh.subscribe("explore/frontiers", 1,
-			&AdditionsServiceProvider::frontierCallback, this);
-	exploration_goals_publisher = nh.advertise<geometry_msgs::PoseArray>(
-			"explorationGoals", 1);
+	_exploration_mode = 0;
+	_goal_obsolete = false;
 }
 
 AdditionsServiceProvider::~AdditionsServiceProvider() {
@@ -56,7 +71,13 @@ AdditionsServiceProvider::~AdditionsServiceProvider() {
 }
 
 void AdditionsServiceProvider::publishTopics() {
-	publishExplorationGoals();
+	if (_calculate_goal_plugin_used) {
+		publishExplorationGoals();
+		publishFailedGoals();
+	}
+	if (_exploration_mode) {
+		publishGoalObsolete();
+	}
 }
 
 bool AdditionsServiceProvider::setNavigationToReverse(
@@ -86,7 +107,28 @@ void AdditionsServiceProvider::navigationGoalCallback(
 void AdditionsServiceProvider::publishExplorationGoals() {
 	_exploration_goals.header.frame_id = "map";
 	_exploration_goals.header.stamp = ros::Time::now();
-	exploration_goals_publisher.publish(_exploration_goals);
+	_exploration_goals_publisher.publish(_exploration_goals);
+}
+
+void AdditionsServiceProvider::publishFailedGoals() {
+	_failed_goals.header.frame_id = "map";
+	_failed_goals.header.stamp = ros::Time::now();
+	_failed_goals_publisher.publish(_failed_goals);
+}
+
+void AdditionsServiceProvider::publishGoalObsolete() {
+	std_msgs::Bool msg;
+	msg.data = _goal_obsolete;
+	_goal_obsolete_publisher.publish(msg);
+}
+
+void AdditionsServiceProvider::explorationGoalCallback(
+		const rsm_msgs::GoalStatus::ConstPtr& goal_status) {
+	if (goal_status->goal_status == rsm_msgs::GoalStatus::REACHED) {
+		_failed_goals.poses.clear();
+	} else if (goal_status->goal_status == rsm_msgs::GoalStatus::FAILED) {
+		_failed_goals.poses.push_back(goal_status->goal);
+	}
 }
 
 void AdditionsServiceProvider::frontierCallback(
@@ -105,13 +147,54 @@ void AdditionsServiceProvider::frontierCallback(
 			}
 		}
 	}
+	if (_exploration_mode && !navGoalIncludedInFrontiers()) {
+		_goal_obsolete = true;
+	} else {
+		_goal_obsolete = false;
+	}
 }
 
-bool AdditionsServiceProvider::resetKinectPosition(
+void AdditionsServiceProvider::explorationModeCallback(
+		const std_msgs::Bool::ConstPtr& exploration_mode) {
+	_exploration_mode = exploration_mode->data;
+	if (_exploration_mode) {
+		ros::NodeHandle nh("rsm");
+		_goal_obsolete_publisher = nh.advertise<std_msgs::Bool>("goalObsolete",
+				1);
+	} else {
+		_goal_obsolete_publisher.shutdown();
+	}
+}
+
+bool AdditionsServiceProvider::navGoalIncludedInFrontiers() {
+	ROS_INFO_STREAM("Goal tolerance: " << _exploration_goal_tolerance);
+	rsm_msgs::GetNavigationGoal srv;
+	if (_get_navigation_goal_service.call(srv)) {
+		geometry_msgs::Pose navigation_goal = srv.response.goal;
+		for (auto iterator : _exploration_goals.poses) {
+			double x_dif = abs(
+					navigation_goal.position.x - iterator.position.x);
+			double y_dif = abs(
+					navigation_goal.position.y - iterator.position.y);
+			double z_dif = abs(
+					navigation_goal.position.z - iterator.position.z);
+			if (x_dif <= _exploration_goal_tolerance
+					&& y_dif <= _exploration_goal_tolerance
+					&& z_dif <= _exploration_goal_tolerance) {
+				return true;
+			}
+		}
+	} else {
+		ROS_ERROR("Failed to call Get Navigation Goal service");
+	}
+	return false;
+}
+
+bool AdditionsServiceProvider::resetRealsensePosition(
 		std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
-	std_msgs::Float64 kinetic_command;
-	kinetic_command.data = 0.0;	//center position of kinect camera
-	_kinetic_joint_controller.publish(kinetic_command);
+	std_msgs::Float64 realsense_command;
+	realsense_command.data = 0.0;	//center position of realsense camera
+	_realsense_joint_controller.publish(realsense_command);
 	return true;
 }
 }
--- rsm_rqt_plugins/CHANGELOG.rst
+++ rsm_rqt_plugins/CHANGELOG.rst
@@ -2,6 +2,13 @@
 Changelog for package rsm_rqt_plugins
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+1.2.0 (2020-11-04)
+------------------
+* Added prefix to state name to identify autonomy behavior
+* Removed double maintainer tags
+* Debounced operation mode buttons
+* Contributors: MarcoStb1993
+
 1.1.3 (2019-08-29)
 ------------------
 * Added all dependencies to CMakeLists and package.xml, that were missing previously
